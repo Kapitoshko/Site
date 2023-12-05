68e029f6c901b8c5cdd09014520c355924261519
Kapitoshko <100803683+Kapitoshko@users.noreply.github.com>
2023-12-06 00:38:59 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # By Dimokus (https://t.me/Dimokus)
 source $HOME/.bashrc
-TZ=Europe/Kiev
-ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-apt-get update
-apt-get upgrade -y
 apt-get install -y sudo nano wget tar zip unzip jq ssh nginx git
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 (echo ${my_root_password}; echo ${my_root_password}) | passwd root
@@ -14,4 +10,3 @@ sleep 5
 rm /var/www/html/index.nginx-debian.html
 cd /var/www/html/
 git clone https://github.com/PavloDereniuk/goit-markup-hw-07 .
-
